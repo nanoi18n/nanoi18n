3bf8bd8f77de2a03bc866dc55ec63f775a2a9e0d
Eric Vera <6858503+ericvera@users.noreply.github.com>
2023-12-05 19:40:54 -0500

    test: complete tests for loadL10n

--- src/loadL10n.test.ts
+++ src/loadL10n.test.ts
@@ -41,7 +41,79 @@ const importersWithMixedParams: NanoI18nL10nImporters<
 		(await import('./__test__/messages-mixed-params.es.js')).messages,
 }
 
-test('types work for importers where all values are strings', async () => {
+const importersSingleLocale: NanoI18nL10nImporters<'es', typeof esMixedParams> =
+	{
+		['es']: async () =>
+			(await import('./__test__/messages-mixed-params.es.js')).messages,
+	}
+
+test('loads the value as expected (all values are strings)', async () => {
+	const l = await loadL10n('en', importersWithNoParams)
+	const result = l('a.0')
+
+	expect(result).toMatchInlineSnapshot('"a.0 en"')
+})
+
+test('loads the value as expected (all values are functions)', async () => {
+	const l = await loadL10n('es', importersWithParams)
+	const result = l('a.1', { b: 'b value' })
+
+	expect(result).toMatchInlineSnapshot('"a.1 es = b value"')
+})
+
+test('loads the value as expected (string in mixed values)', async () => {
+	const l = await loadL10n('es', importersWithMixedParams)
+	const result = l('a.1')
+
+	expect(result).toMatchInlineSnapshot('"a.1 es"')
+})
+
+test('loads the value as expected (function in mixed values)', async () => {
+	const l = await loadL10n('en', importersWithMixedParams)
+	const result = l('a.0', { a: 'hi! I am a!' })
+
+	expect(result).toMatchInlineSnapshot('"a.0 en = hi! I am a!"')
+})
+
+test('loads the value when there is a single language included in the importer', async () => {
+	const l = await loadL10n('es', importersSingleLocale)
+	const result = l('a.0', { a: 'hola hola!' })
+
+	expect(result).toMatchInlineSnapshot('"a.0 es = hola hola!"')
+})
+
+test('l function throws when there is no param and it is expected', async () => {
+	const l = await loadL10n('es', importersWithParams)
+
+	expect(() => {
+		// @ts-expect-error Expect TS to complain about missing params
+		l('a.1')
+	}).toThrowErrorMatchingInlineSnapshot(
+		"\"Params for key 'a.1' in locale 'es' unexpectedly not found.\"",
+	)
+})
+
+test('l function throws when there is no message for specified key', async () => {
+	const l = await loadL10n('en', importersWithNoParams)
+
+	expect(() => {
+		// @ts-expect-error Expect TS to complain about invalid key
+		l('c', { a: '0' })
+	}).toThrowErrorMatchingInlineSnapshot(
+		"\"Message for key 'c' in locale 'en' unexpectedly has type 'undefined'.\"",
+	)
+})
+
+test('l function throws when the requested locale is not included in the importers', async () => {
+	await expect(async () => {
+		// @ts-expect-error Expect TS to complain about invalid key
+		await loadL10n('fr', importersWithNoParams)
+	}).rejects.toThrowErrorMatchingInlineSnapshot(
+		'"Requested locale \'fr\' not included in importers argument."',
+	)
+})
+
+test('TS Errors happen on incorrect keys/parameters for importers (all values are strings)', async () => {
 	const l = await loadL10n('es', importersWithNoParams)
 
 	// NOTE: The following tests are mainly about types being correct. In the case
@@ -59,19 +131,12 @@ test('types work for importers where all values are strings', async () => {
 	}).toThrowError()
 })
 
-test('l function throws when there is no message for specified key', async () => {
-	const l = await loadL10n('en', importersWithNoParams)
-
-	expect(() => {
-		// @ts-expect-error Expect TS to complain about invalid key
-		l('c', { a: '0' })
-	}).toThrowErrorMatchingInlineSnapshot(
-		"\"Message for key 'c' in locale 'en' unexpectedly has type 'undefined'.\"",
-	)
-})
-
-test('types work for importers where all values are parameterized functions', async () => {
+test('TS Errors happen on incorrect keys/parameters for importers (all values are parameterized functions)', async () => {
 	const l = await loadL10n('en', importersWithParams)
+
+	// NOTE: The following tests are mainly about types being correct. In the case
+	// of any changes make sure that the @ts-expect-error matches the actual
+	// error.
 
 	expect(() => {
 		// @ts-expect-error Expect TS to complain about k to not be a valid parameter name
@@ -99,19 +164,12 @@ test('types work for importers where all values are parameterized functions', as
 	}).not.toThrow()
 })
 
-test('l function throws when there is param and it is expected', async () => {
-	const l = await loadL10n('es', importersWithParams)
-
-	expect(() => {
-		// @ts-expect-error Expect TS to complain about missing params
-		l('a.1')
-	}).toThrowErrorMatchingInlineSnapshot(
-		"\"Params for key 'a.1' in locale 'es' unexpectedly not found.\"",
-	)
-})
-
-test('types work for importers where all values are a mix of strings and parameterized functions', async () => {
+test('TS Errors happen on incorrect keys/parameters for importers (mixed values include parameterized functions and strings)', async () => {
 	const l = await loadL10n('es', importersWithMixedParams)
+
+	// NOTE: The following tests are mainly about types being correct. In the case
+	// of any changes make sure that the @ts-expect-error matches the actual
+	// error.
 
 	expect(() => {
 		// @ts-expect-error Expect TS to complain about missing params
@@ -138,30 +196,3 @@ test('types work for importers where all values are a mix of strings and paramet
 		l('a.1', { b: 'b' })
 	}).not.toThrow()
 })
-
-test("types on call to laodL10n result in error when the keys don't match", async () => {
-	// TODO: Fix
-})
-
-test('types on call to laodL10n result in error when the value for the given key does not match (function vs string)', async () => {
-	// TODO: Fix
-})
-
-test('types on call to laodL10n result in error when the value for the given key does not match (both functions, but different param props names)', async () => {
-	// TODO: Fix
-})
-
-test('types on call to laodL10n result in error when the value for the given key does not match (both functions where one of the functions contains an extra prop)', async () => {
-	// TODO: Fix
-})
-
-test('loads the function as expected', async () => {
-	// TODO: Fix
-
-	const l = await loadL10n('en', importersWithNoParams)
-	const result = l('a.0')
-
-	expect(result).toMatchInlineSnapshot('"a.0 en"')
-})
-
-// TODO: Add tests
